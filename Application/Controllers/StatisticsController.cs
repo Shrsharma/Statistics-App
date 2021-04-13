0bb94bbe726ad53a7ed92b58b9c3a256b9ea8f69
shrsharma <shrsharma@HSC-6JVKLC2>
2021-04-13 23:51:34 +0530

    Statistics changes

--- Application/Controllers/StatisticsController.cs
+++ Application/Controllers/StatisticsController.cs
@@ -15,26 +15,87 @@ namespace Application.Controllers
 
     public class StatisticsController : Controller
     {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
         [Route("")]
-        public IActionResult Index()
+        public ActionResult Index()
         {
-            return View();
-        }
+            List<StudentModel> student = (List<StudentModel>) GetStud();
 
-        [Route("opr")]
-        public ActionResult Opeartion()
-        {
-            List<StudentModel> student = GetStud();
-            List<SubjectModel> subject = new List<SubjectModel>();
-
+            var StudCnt = student.Count();
+            var maxScience = student.Max(u => u.Science);
+            var maxMaths = student.Max(u => u.Maths);
+            var maxEnglish = student.Max(u => u.English);
             var minScience = student.Min(u => u.Science);
+            var minMaths = student.Min(u => u.Maths);
+            var minEnglish = student.Min(u => u.English);
+            var avgScience = student.Average(u => long.Parse(u.Science));
+            var avgMaths = student.Min(u => long.Parse(u.Maths));
+            var avgEnglish = student.Min(u => long.Parse(u.English));
 
-            HttpContext.Session.SetString("Mins", minScience);
+            Response.Cookies.Append("StudCnt", StudCnt.ToString());
+            Response.Cookies.Append("maxScience", maxScience);
+            Response.Cookies.Append("maxMaths", maxMaths);
+            Response.Cookies.Append("maxEnglish", maxEnglish);
+            Response.Cookies.Append("minScience", minScience);
+            Response.Cookies.Append("minMaths", minMaths);
+            Response.Cookies.Append("minEnglish", minEnglish);
+            Response.Cookies.Append("avgScience", avgScience.ToString());
+            Response.Cookies.Append("avgMaths", avgMaths.ToString());
+            Response.Cookies.Append("avgEnglish", avgEnglish.ToString());
+
+            var mxsStud = from stud in student
+                           where stud.Science == maxScience
+                           select new { StudName1 = stud.Name};
+            foreach(var s in mxsStud)
+            {
+                Response.Cookies.Append("MaxStudScience", s.StudName1);
+            }
+            var mxeStud = from stud1 in student
+                          where stud1.English == maxEnglish
+                          select new { StudName2 = stud1.Name };
+            foreach (var s in mxeStud)
+            {
+                Response.Cookies.Append("MaxStudEnglish", s.StudName2);
+            }
+            var mxmStud = from stud2 in student
+                          where stud2.Maths == maxMaths
+                          select new { StudName3 = stud2.Name };
+            foreach (var s in mxmStud)
+            {
+                Response.Cookies.Append("MaxStudMaths", s.StudName3);
+            }
+
+            var mnsStud = from s1 in student
+                          where s1.Science == minScience
+                          select new { S1Name = s1.Name };
+            foreach( var s in mnsStud)
+            {
+                Response.Cookies.Append("MinStudScience", s.S1Name);
+            }
+            var mneStud = from s2 in student
+                          where s2.English == minEnglish
+                          select new { S2Name = s2.Name };
+            foreach( var s in mneStud)
+            {
+                Response.Cookies.Append("MinStudEnglish", s.S2Name);
+            }
+            var mnmStud = from s3 in student
+                          where s3.Maths == minMaths
+                          select new { S3Name = s3.Name };
+            foreach(var s in mnmStud)
+            {
+                Response.Cookies.Append("MinStudMaths", s.S3Name);
+            }
 
             return View();
         }
 
-        public List<StudentModel> GetStud()
+        [Route("Rare")]
+        public IEnumerable<StudentModel> GetStud()
         {
             string connectionstring = "Server=HSC-6JVKLC2\\SQLEXPRESS; Database=StatDb; Trusted_Connection=True;";
 
